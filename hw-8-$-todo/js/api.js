549b04db02e87876ffab1f9ef0664f7e651f0f2a
katrysja <catherine.slepchenko@gmail.com>
2022-01-16 18:01:35 +0200

    hillel/hw-8-jQuery

---
+++ hw-8-$-todo/js/api.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export function getTodoList() {
+    return $.get(`${BASE_URL}/todos`);
+}
+
+export function getTodoListItem(id) {
+    return $.ajax(`${BASE_URL}/todos/${id}`);
+}
+
+export function patchTodoListItem(id, data) {
+    return $.ajax({
+        type: 'PATCH',
+        url: `${BASE_URL}/todos/${id}`,
+        data
+    });
+}
+
+export function deleteTodoListItem(id) {
+    return $.ajax({
+        type: 'DELETE',
+        url: `${BASE_URL}/todos/${id}`
+    });
+}
